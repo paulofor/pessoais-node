ee6aed90167635a0666e0d5c9c6e1ca31486778c
Paulo Alexandre <paulofore@gmail.com>
2022-12-14 12:15:49 -0300

    semanal

--- server-pessoais/gerproj-server/common/models/ger-proj-tempo-tarefa.js
+++ server-pessoais/gerproj-server/common/models/ger-proj-tempo-tarefa.js
@@ -76,7 +76,8 @@ module.exports = function(Gerprojtempotarefa) {
             " inner join iteracao_entrega on id_iteracao_entrega = id_iteracao_entrega_cp " +
             " inner join entrega_projeto on id_entrega_projeto = iteracao_entrega.id_entrega_projeto_ra " +
             " where entrega_projeto.id_projeto_pmbok_ee = " + idProjeto +
-            " and tempo_tarefa.DATA = date( DATE_SUB(NOW(), INTERVAL 1 HOUR)) " +
+            //" and tempo_tarefa.DATA = date( DATE_SUB(NOW(), INTERVAL 1 HOUR)) " +
+            " and tempo_tarefa.DATA = date(now()) " +
             " order by DATA, hora_inicio desc " +
             " limit 1";
         let ds = Gerprojtempotarefa.dataSource;
